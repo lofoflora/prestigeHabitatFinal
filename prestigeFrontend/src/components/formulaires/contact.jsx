120dccfbe496a444335c4181a890595e8c002d30
lofoflora <lofoflora@gmail.com>
2023-09-26 12:07:31 +0200

    form contact

---
+++ prestigeFrontend/src/components/formulaires/contact.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Ici, tu peux ajouter la logique pour envoyer le formulaire de contact.
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h2>Contactez-nous</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Nom :</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email :</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="subject">Objet :</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message :</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
